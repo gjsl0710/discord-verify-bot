5432951609b63817eb3320ee95d33c95836a673b
gjsl0710 <67987471+gjsl0710@users.noreply.github.com>
2020-10-03 19:30:09 +0900

    Update inj.py

--- inj.py
+++ inj.py
@@ -13,12 +13,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("인증 봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('★★~하는중에 표시될 게임★★')
+    game = discord.Game('24시간구동')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("/인증"): #명령어 /인증
+    if message.content.startswith("!인증"): #명령어 /인증
         a = ""
         Captcha_img = ImageCaptcha()
         for i in range(6):
@@ -45,7 +45,7 @@ async def on_message(message):
             return
 
         if msg.content == a:
-            role = discord.utils.get(message.guild.roles, name="★★역 = 할 = 이 = 름★★")
+            role = discord.utils.get(message.guild.roles, name="클랜원")
             await message.channel.purge(limit=4)
             tjdrhdEmbed = discord.Embed(title='인증성공', color=0x04FF00)
             tjdrhdEmbed.add_field(name='닉네임', value=message.author, inline=False)
